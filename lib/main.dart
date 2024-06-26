65e6d17ca8cb5ca679f777890c5bdbf93a3a37ba
buytango03 <buytango03@gmail.com>
2024-06-26 13:50:36 +0000

    Added controller into main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/view/pick_image.dart';
 import 'package:camera/camera.dart';
+import 'package:myapp/model/camera_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final cameras = await availableCameras();
   final firstCamera = cameras.first;
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:1015538657.
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CameraProvider()..setCamera(firstCamera),
+      child: const MyApp(),
+    ),
+  );
 }
 
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:3428824773.
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:2762535592.
 class MyApp extends StatelessWidget {
 
   const MyApp({super.key});
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:3961586953.
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:1348398641.
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
